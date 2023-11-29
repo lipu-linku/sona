1e31a843e755fe7f265ad09d7b806d60046f22bd
theonlytails <theonlytails@theonlytails.com>
2023-11-29 21:10:04 +0200

    added translation file json schema and schema validation in workflow

--- .github/workflows/validate_toml.py
+++ .github/workflows/validate_toml.py
@@ -1,11 +1,18 @@
 from glob import iglob
-from tomllib import load, TOMLDecodeError
+import tomllib
+from jsonschema import validate
+import json
 
 if __name__ == "__main__":
   for path in iglob("../../**/*.toml", recursive=True):
     with open(path, 'rb') as file:
       try:
         print(f"Checking {file.name}...")
-        load(file)
-      except TOMLDecodeError as e:
+        data = tomllib.load(file)
+        
+        if "translations" in path:
+          with open("../../schemas/translation.schema.json") as schema:
+            print(f"Checking translation schema for {file.name}...")
+            validate(data, json.load(schema))
+      except tomllib.TOMLDecodeError as e:
         print(e)
