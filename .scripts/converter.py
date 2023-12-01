d2800a67b7aa01188c11309643bd79ee6b5ecfb6
Gregory Danielson <gregory.danielson3@gmail.com>
2023-12-01 11:27:27 -0600

    Split translatable fields into files per type of thing to translate
    This includes splitting the etymology fields into their translatable and untranslatable components (words and alts -> static data; definition and language -> etymology.toml)

--- .scripts/converter.py
+++ .scripts/converter.py
@@ -24,7 +24,10 @@ def transform_ku_data(word: str, data: dict):
 def transform_etym_data(word: str, data: dict):
     if not data:
         return []
-    etyms = []
+    transable_etyms = []
+
+    transable_etyms = []
+    untransable_etyms = []
 
     langs = data.get("langs", "").split(";")
     defs = data.get("defs", "").split(";")
@@ -32,17 +35,19 @@ def transform_etym_data(word: str, data: dict):
     alts = data.get("alts", "").split(";")
 
     for lang, _def, word, alt in zip(langs, defs, words, alts):
-        new_etym = dict()
+        transable = dict()
+        untransable = dict()
         if lang:
-            new_etym["language"] = lang
+            transable["language"] = lang
         if _def:
-            new_etym["definition"] = _def
+            transable["definition"] = _def
         if word:
-            new_etym["word"] = word
+            untransable["word"] = word
         if alt:
-            new_etym["alt"] = alt
-        etyms.append(new_etym)
-    return etyms
+            untransable["alt"] = alt
+        transable_etyms.append(transable)
+        untransable_etyms.append(untransable)
+    return transable_etyms, untransable_etyms
 
 
 def transform_recognition_data(word: str, data: dict):
@@ -62,6 +67,14 @@ def noop(word: str, data: dict, _return_if_null: Any = ""):
 
 def trash(word: str, data: dict):
     return None
+
+
+WORDS = nested_defaultdict()
+DEFINITIONS = nested_defaultdict()
+
+COMMENTARY = nested_defaultdict()
+SP_ETYMOLOGY = nested_defaultdict()
+ETYMOLOGY = nested_defaultdict()
 
 
 TRANSFORM_MAP = {
@@ -91,11 +104,31 @@ TRANSFORM_MAP = {
     "def": trash,
 }
 
+TRANSFORMER = "t"
+DESTINATION = "d"
+
 TRANSLATION_MAP = {
-    "etymology_data": transform_etym_data,
-    "commentary": noop,
-    "sitelen_pona_etymology": noop,
+    "etymology_data": {
+        TRANSFORMER: transform_etym_data,
+        DESTINATION: ETYMOLOGY,
+    },
+    "commentary": {
+        TRANSFORMER: noop,
+        DESTINATION: COMMENTARY,
+    },
+    "sitelen_pona_etymology": {
+        TRANSFORMER: noop,
+        DESTINATION: SP_ETYMOLOGY,
+    },
 }
+
+
+def write_translated(data: dict, dir: str, filename: str):
+    for lang, d in data.items():
+        os.makedirs(f"{dir}/{lang}", exist_ok=True)
+        with open(f"{dir}/{lang}/{filename}", "w") as f:
+            tomlified = tomlkit.dumps(d, sort_keys=True)
+            f.write(tomlified)
 
 
 def main():
@@ -107,49 +140,50 @@ def main():
         langs = jasima["languages"]
         data = jasima["data"]
 
-    words = nested_defaultdict()
-    translations = nested_defaultdict()
     for word in data.keys():
         for field in TRANSFORM_MAP.keys():
             fetched = data[word].get(field)
             formatted = TRANSFORM_MAP[field](word, fetched)
             if formatted is not None:
-                words[word][field] = formatted
+                WORDS[word][field] = formatted
+
+            # if field == "ucsur":
+            #     codepoint = data[word].get("ucsur")
+            #     character = ""
+            #     if codepoint:
+            #         character = chr(int(codepoint[2:], base=16))
+            #     words[word]["ucsur_codepoint"] = codepoint
+            #     words[word]["ucsur_character"] = character
+            #     continue
 
         for lang in langs.keys():
-            translations[lang][word]["def"] = data[word]["def"].get(lang) or ""
+            DEFINITIONS[lang][word] = data[word]["def"].get(lang) or ""
             for field in TRANSLATION_MAP:
                 fetched = data[word].get(field)
-                formatted = TRANSLATION_MAP[field](word, fetched)
+                formatted = TRANSLATION_MAP[field][TRANSFORMER](word, fetched)
 
                 # TODO: key-aware transform
-                if field == "etymology_data":
-                    field = "etymology"
 
                 if formatted is not None:
-                    translations[lang][word][field] = formatted
-
-                # if field == "ucsur":
-                #     codepoint = data[word].get("ucsur")
-                #     character = ""
-                #     if codepoint:
-                #         character = chr(int(codepoint[2:], base=16))
-                #     words[word]["ucsur_codepoint"] = codepoint
-                #     words[word]["ucsur_character"] = character
-                #     continue
+                    write_to = TRANSLATION_MAP[field][DESTINATION]
+                    if field == "etymology_data":
+                        untransable = formatted[1] if formatted else []
+                        formatted = formatted[0] if formatted else []
+                        field = "etymology"
+                        WORDS[word][field] = untransable
+                    write_to[lang][word] = formatted
 
     # TODO: order keys freely instead of alphabetically
     # or crowdin will solve this for us
-    for word, worddata in words.items():
+    for word, worddata in WORDS.items():
         with open(f"../words/{word}.toml", "w") as f:
             tomlified = tomlkit.dumps(worddata, sort_keys=True)
             f.write(tomlified)
 
-    for lang, translationdata in translations.items():
-        # TODO: fix Pingo being at the top
-        tomlified = tomlkit.dumps(translationdata, sort_keys=True)
-        with open(f"../translations/{lang}.toml", "w") as f:
-            f.write(tomlified)
+    write_translated(DEFINITIONS, "../translations", "definitions.toml")
+    write_translated(COMMENTARY, "../translations", "commentary.toml")
+    write_translated(ETYMOLOGY, "../translations", "etymology.toml")
+    write_translated(SP_ETYMOLOGY, "../translations", "sp_etymology.toml")
 
 
 if __name__ == "__main__":
