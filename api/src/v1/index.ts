550686164e4118f2e3c9b5bd0fde729dca161323
theonlytails <theonlytails@theonlytails.com>
2023-12-11 04:31:52 +0200

    added /word/:word route to the api with localization

--- api/src/v1/index.ts
+++ api/src/v1/index.ts
@@ -1,29 +1,25 @@
 import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
-import { BASE_URL, branches } from "../versioning";
-import { Data } from "@linku/sona";
-import { fromZodError } from "zod-validation-error";
-
-const branch = branches.v1;
+import { getData } from "../versioning";
 
 const app = new Hono();
 
-app.get("/data", async (c) => {
-	const data = await fetch(`${BASE_URL}/${branch}/raw/data.json`).then((r) => r.json());
-
-	const parseRes = await Data.safeParseAsync(data);
-
-	if (parseRes.success) return c.json(parseRes.data);
-	else
-		throw new HTTPException(500, {
-			message: `Internal server error: ${fromZodError(parseRes.error)}`,
-		});
+app.get("/words", async (c) => {
+	return c.json(await getData("v1"));
 });
 
 app.get("/word/:word", async (c) => {
-	const word = c.req.param("word");
+	const data = await getData("v1");
+	const word = data[c.req.param("word")];
+	if (!word) return c.notFound();
+
 	const languages = c.req.query("lang")?.split(",") ?? ["en"];
-	// const data = 	
+
+	return c.json({
+		...word,
+		translations: Object.fromEntries(
+			Object.entries(word.translations).filter(([key, _]) => languages.includes(key)),
+		),
+	});
 });
 
 export default app;
--- api/src/versioning.ts
+++ api/src/versioning.ts
@@ -1,5 +1,42 @@
+import {
+	CommentaryTranslation,
+	Data,
+	DefinitionTranslation,
+	EtymologyTranslation,
+	SitelenPonaTranslation,
+	Word,
+} from "@linku/sona";
+import { HTTPException } from "hono/http-exception";
+import type { z } from "zod";
+import { fromZodError } from "zod-validation-error";
+
 export const BASE_URL = "https://raw.githubusercontent.com/lipu-linku/sona";
 
-export const branches = {
-	v1: "api",
+export const versions = {
+	v1: {
+		branch: "api",
+		schemas: {
+			data: Data,
+			word: Word,
+			definition: DefinitionTranslation,
+			commentary: CommentaryTranslation,
+			etymology: EtymologyTranslation,
+			sitelen_pona: SitelenPonaTranslation,
+		},
+	},
 } as const;
+
+export const getData = async <V extends keyof typeof versions>(
+	version: V,
+): Promise<z.infer<(typeof versions)[V]["schemas"]["data"]>> => {
+	const data = await fetch(`${BASE_URL}/${versions[version].branch}/raw/data.json`).then((r) =>
+		r.json(),
+	);
+	const parseRes = await versions[version].schemas.data.safeParseAsync(data);
+
+	if (parseRes.success) return parseRes.data;
+	else
+		throw new HTTPException(500, {
+			message: `Internal server error: ${fromZodError(parseRes.error)}`,
+		});
+};
