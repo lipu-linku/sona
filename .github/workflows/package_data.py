62f85fc8762eb4b84e3899c3a0f5ebf8be4e1962
theonlytails <theonlytails@theonlytails.com>
2024-01-12 00:47:59 +0200

    made the schemas match the data reordered the entire folder structure to be more unified and clear reworked the packaging script to account for multiple output types

--- .github/workflows/package_data.py
+++ .github/workflows/package_data.py
@@ -1,36 +1,103 @@
 import glob
+import itertools
 import json
 import os
-from typing import Any
+from typing import Any, Iterator
 
 import tomlkit
 
-if __name__ == "__main__":
-    result: dict[str, Any] = {}
+data_types: dict[str, str | list[str]] = {
+    "words": "metadata",
+    "luka_pona": ["signs", "fingerspelling"],
+}
 
-    for path in glob.iglob("./words/*.toml"):
+
+def extract_data(
+    result: dict[str, Any],
+    paths: Iterator[str],
+    data_type: str,
+    subpath: bool,
+):
+    for path in paths:
         with open(path) as file:
             print(f"Reading {path}...")
-            word_data = tomlkit.load(file)
+            raw_data = tomlkit.load(file)
             id = os.path.basename(path[: path.index(".toml")])
+            data = {key: value for (key, value) in dict(raw_data).items()}
 
-            result[id] = {key: value for (key, value) in dict(word_data).items()}
+            if subpath:
+                prefix = f"./{data_type}/"
+                folder = path[
+                    path.index(prefix) + len(prefix) : path.index(f"/{id}.toml")
+                ]
+                result[folder][id] = data
+            else:
+                result[id] = data
+
             result[id]["translations"] = {}
 
-    for path in glob.iglob("./translations/*/*.toml"):
+
+def insert_translations(
+    result: dict[str, Any], paths: Iterator[str], data_type: str, subpath: bool
+):
+    for path in paths:
         with open(path) as file:
             print(f"Reading {path}...")
             localized_data = tomlkit.load(file)
             locale = os.path.dirname(path)[path.rfind("/", 0, path.rfind("/")) + 1 :]
             data_kind = os.path.basename(path[: path.index(".toml")])
 
-            for word in {word for word in localized_data.keys()}:
-                if locale not in result[word]["translations"]:
-                    result[word]["translations"][locale] = {}
+        for item in {item for item in localized_data.keys()}:
+            if subpath:
+                prefix = f"./{data_type}/"
+                folder = path[
+                    path.index(prefix) + len(prefix) : path.index(
+                        f"/translations/{item}.toml"
+                    )
+                ]
+                if locale not in result[folder][item]["translations"]:
+                    result[folder][item]["translations"][locale] = {}
 
-                result[word]["translations"][locale][data_kind] = localized_data[word]
+                result[folder][item]["translations"][locale][
+                    data_kind
+                ] = localized_data[item]
+            else:
+                if locale not in result[item]["translations"]:
+                    result[item]["translations"][locale] = {}
 
-    with open("raw/data.json", "w+") as data_file:
-        data_file.write(json.dumps(result, separators=(",", ":")))
+                result[item]["translations"][locale][data_kind] = localized_data[item]
+
+
+if __name__ == "__main__":
+    for data_type, folders in data_types.items():
+        result: dict[str, Any] = {}
+
+        extract_data(
+            result,
+            glob.iglob(f"./{data_type}/{folders}/*.toml")
+            if isinstance(folders, str)
+            else itertools.chain(
+                *(glob.iglob(f"./{data_type}/{folder}/*.toml") for folder in folders)
+            ),
+            data_type,
+            not isinstance(folders, str),
+        )
+
+        insert_translations(
+            result,
+            glob.iglob(f"./{data_type}/{folders}/translations/*.toml")
+            if isinstance(folders, str)
+            else itertools.chain(
+                *(
+                    glob.iglob(f"./{data_type}/{folder}/translations/*.toml")
+                    for folder in folders
+                )
+            ),
+            data_type,
+            not isinstance(folders, str),
+        )
+
+        with open(f"raw/{data_type}.json", "w+") as data_file:
+            data_file.write(json.dumps(result, separators=(",", ":")))
 
     print("Done!")
