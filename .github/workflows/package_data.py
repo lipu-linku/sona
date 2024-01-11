3af3658fc9326ca86a05efd3ce19ce0a5a526a55
TheOnlyTails <theonlytails@theonlytails.com>
2024-01-11 16:15:01 +0200

    schema validation is now done via a taplo directive (#14)
    * schema validation is now done via a taplo directive
    
    * stop waiting for schemas to generate when packaging data files
    
    ---------
    
    Co-authored-by: TheOnlyTails <TheOnlyTails@users.noreply.github.com>

--- .github/workflows/package_data.py
+++ .github/workflows/package_data.py
@@ -1,14 +1,11 @@
-from typing import Any
-import tomlkit
-import json
 import glob
+import json
 import os
-import sys
-import urllib.parse
+from typing import Any
+
+import tomlkit
 
 if __name__ == "__main__":
-    assert len(sys.argv) == 2
-
     result: dict[str, Any] = {}
 
     for path in glob.iglob("./words/*.toml"):
@@ -17,11 +14,7 @@ if __name__ == "__main__":
             word_data = tomlkit.load(file)
             id = os.path.basename(path[: path.index(".toml")])
 
-            result[id] = {
-                key: value
-                for (key, value) in dict(word_data).items()
-                if key != "$schema"
-            }
+            result[id] = {key: value for (key, value) in dict(word_data).items()}
             result[id]["translations"] = {}
 
     for path in glob.iglob("./translations/*/*.toml"):
@@ -31,16 +24,13 @@ if __name__ == "__main__":
             locale = os.path.dirname(path)[path.rfind("/", 0, path.rfind("/")) + 1 :]
             data_kind = os.path.basename(path[: path.index(".toml")])
 
-            for word in (word for word in localized_data.keys() if word != "$schema"):
+            for word in {word for word in localized_data.keys()}:
                 if locale not in result[word]["translations"]:
                     result[word]["translations"][locale] = {}
 
                 result[word]["translations"][locale][data_kind] = localized_data[word]
 
     with open("raw/data.json", "w+") as data_file:
-        result[
-            "$schema"
-        ] = f"https://raw.githubusercontent.com/lipu-linku/sona/{urllib.parse.quote(sys.argv[1])}/schemas/generated/data.json"
         data_file.write(json.dumps(result, separators=(",", ":")))
 
     print("Done!")
