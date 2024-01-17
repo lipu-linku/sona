fa8e9af574cead30234de8d293a45899181d03e9
theonlytails <theonlytails@theonlytails.com>
2024-01-17 17:50:10 +0200

    The directory structure of all the data folders is now the same The packaging workflow now iterates over all the files instead of manually specifying ones luka_pona.json has been split to signs.json and fingerspelling.json (including schemas)

--- .github/workflows/package_data.py
+++ .github/workflows/package_data.py
@@ -1,22 +1,23 @@
 import glob
-import itertools
 import json
 import os
-from typing import Any, Iterator
+from typing import Any, Final, Iterator
 
 import tomlkit
 
-data_types: dict[str, str | list[str]] = {
-    "words": "metadata",
-    "luka_pona": ["signs", "fingerspelling"],
+data_folder: Final[str] = "metadata"
+translations_folder: Final[str] = "translations"
+
+data_types: Final[set[str]] = {
+    "words",
+    "luka_pona/signs",
+    "luka_pona/fingerspelling",
 }
 
 
 def extract_data(
     result: dict[str, Any],
     paths: Iterator[str],
-    data_type: str,
-    subpath: bool,
 ):
     for path in paths:
         with open(path) as file:
@@ -25,24 +26,11 @@ def extract_data(
             id = os.path.basename(path[: path.index(".toml")])
             data = {key: value for (key, value) in dict(raw_data).items()}
 
-            if subpath:
-                prefix = f"./{data_type}/"
-                folder = path[
-                    path.index(prefix) + len(prefix) : path.index(f"/{id}.toml")
-                ]
-                if folder not in result:
-                    result[folder] = {}
-
-                result[folder][id] = data
-                result[folder][id]["translations"] = {}
-            else:
-                result[id] = data
-                result[id]["translations"] = {}
+            result[id] = data
+            result[id]["translations"] = {}
 
 
-def insert_translations(
-    result: dict[str, Any], paths: Iterator[str], data_type: str, subpath: bool
-):
+def insert_translations(result: dict[str, Any], paths: Iterator[str]):
     for path in paths:
         with open(path) as file:
             print(f"Reading {path}...")
@@ -51,57 +39,25 @@ def insert_translations(
             data_kind = os.path.basename(path[: path.index(".toml")])
 
             for item in {item for item in localized_data.keys()}:
-                if subpath:
-                    prefix = f"./{data_type}/"
-                    print(path)
-                    folder = path[
-                        path.index(prefix) + len(prefix) : path.index(
-                            f"/translations/{locale}/{data_kind}.toml"
-                        )
-                    ]
-                    if locale not in result[folder][item]["translations"]:
-                        result[folder][item]["translations"][locale] = {}
+                if locale not in result[item]["translations"]:
+                    result[item]["translations"][locale] = {}
 
-                    result[folder][item]["translations"][locale][
-                        data_kind
-                    ] = localized_data[item]
-                else:
-                    if locale not in result[item]["translations"]:
-                        result[item]["translations"][locale] = {}
-
-                    result[item]["translations"][locale][data_kind] = localized_data[item]
+                result[item]["translations"][locale][data_kind] = localized_data[item]
 
 
 if __name__ == "__main__":
-    for data_type, folders in data_types.items():
+    for data_type in data_types:
         result: dict[str, Any] = {}
 
-        extract_data(
-            result,
-            glob.iglob(f"./{data_type}/{folders}/*.toml")
-            if isinstance(folders, str)
-            else itertools.chain(
-                *(glob.iglob(f"./{data_type}/{folder}/*.toml") for folder in folders)
-            ),
-            data_type,
-            not isinstance(folders, str),
-        )
+        extract_data(result, glob.iglob(f"./{data_type}/{data_folder}/*.toml"))
 
         insert_translations(
             result,
-            glob.iglob(f"./{data_type}/{folders}/translations/*/*.toml")
-            if isinstance(folders, str)
-            else itertools.chain(
-                *(
-                    glob.iglob(f"./{data_type}/{folder}/translations/*/*.toml")
-                    for folder in folders
-                )
-            ),
-            data_type,
-            not isinstance(folders, str),
+            glob.iglob(f"./{data_type}/{data_folder}/{translations_folder}/*/*.toml"),
         )
 
-        with open(f"./raw/{data_type}.json", "w+") as data_file:
+        raw_filename = data_type[0 if (i := data_type.find("/")) == -1 else i :]
+        with open(f"./raw/{raw_filename}.json", "w+") as data_file:
             data_file.write(json.dumps(result, separators=(",", ":")))
 
     print("Done!")
