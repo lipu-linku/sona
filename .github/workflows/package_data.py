63baa27dfb4b3e1d3e9186464fdaa2a77c89d7ee
theonlytails <theonlytails@theonlytails.com>
2024-01-19 12:42:41 +0200

    fixed raw fonts.json file migrated the packaging script to use pathlib instead of os and glob

--- .github/workflows/package_data.py
+++ .github/workflows/package_data.py
@@ -1,7 +1,6 @@
-import glob
 import json
-import os
 from typing import Any, Final, Iterator
+from pathlib import Path
 
 import tomlkit
 
@@ -12,31 +11,27 @@ DATA_TYPES: Final[set[str]] = {
     "words",
     "luka_pona/signs",
     "luka_pona/fingerspelling",
-    "fonts/",
+    "fonts",
 }
 
 
 def extract_data(
     result: dict[str, Any],
-    paths: Iterator[str],
+    paths: Iterator[Path],
 ):
     for path in paths:
         with open(path) as file:
             print(f"Reading {path}...")
-            raw_data = tomlkit.load(file)
-            id = os.path.basename(path[: path.index(".toml")])
-            data = {key: value for (key, value) in dict(raw_data).items()}
-
-            result[id] = data
+            result[path.stem] = tomlkit.load(file)
 
 
-def insert_translations(result: dict[str, Any], paths: Iterator[str]):
+def insert_translations(result: dict[str, Any], paths: Iterator[Path]):
     for path in paths:
         with open(path) as file:
             print(f"Reading {path}...")
             localized_data = tomlkit.load(file)
-            locale = os.path.dirname(path)[path.rfind("/", 0, path.rfind("/")) + 1 :]
-            data_kind = os.path.basename(path[: path.index(".toml")])
+            locale = path.parent.stem
+            data_kind = path.stem
 
             for item in {item for item in localized_data.keys()}:
                 if "translations" not in result[item]:
@@ -52,15 +47,15 @@ if __name__ == "__main__":
     for data_type in DATA_TYPES:
         result: dict[str, Any] = {}
 
-        extract_data(result, glob.iglob(f"./{data_type}/{DATA_FOLDER}/*.toml"))
+        extract_data(result, Path(".").glob(f"./{data_type}/{DATA_FOLDER}/*.toml"))
 
         insert_translations(
             result,
-            glob.iglob(f"./{data_type}/{TRANSLATIONS_FOLDER}/*/*.toml"),
+            Path(".").glob(f"./{data_type}/{TRANSLATIONS_FOLDER}/*/*.toml"),
         )
 
-        raw_filename = data_type[0 if (i := data_type.find("/")) == -1 else i :]
-        with open(f"./raw/{raw_filename}.json", "w+") as data_file:
+        raw_filename = Path("raw") / Path(data_type).stem
+        with open(raw_filename.with_suffix(".json"), "w+") as data_file:
             data_file.write(json.dumps(result, separators=(",", ":")))
 
     print("Done!")
