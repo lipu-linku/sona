01005036a4e7683eb5f4a3107850c7ccd5c21a2b
theonlytails <theonlytails@theonlytails.com>
2024-01-23 21:00:45 +0200

    throw an error when trying to request a non-existent language

--- api/src/utils.ts
+++ api/src/utils.ts
@@ -1,4 +1,5 @@
 import { MiddlewareHandler } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 export const entries = <T extends object>(o: T) => {
 	return Object.entries(o) as [keyof T, T[keyof T]][];
@@ -35,21 +36,35 @@ export const languagesFilter =
 								(e): e is [string, { translations: any }] =>
 									typeof e[1] === "object" && !!e[1] && "translations" in e[1],
 							)
-							.map(([k, v]) => [
-								k,
-								{
-									...v,
-									translations: filterObject(v["translations"], ([k]) =>
-										languages.includes(k.toString()),
-									),
-								},
-							]),
+							.map(([k, v]) => {
+								const availableLangs = Object.keys(v.translations);
+								if (languages.some((l) => !availableLangs.includes(l)))
+									throw new HTTPException(400, {
+										message: `Cannot find some of the requested languages: ${languages.join(", ")}`,
+									});
+
+								return [
+									k,
+									{
+										...v,
+										translations: filterObject(v["translations"], ([k]) =>
+											languages.includes(k.toString()),
+										),
+									},
+								];
+							}),
 					),
 				),
 				c.res,
 			);
 		} else {
 			if ("translations" in body) {
+				const availableLangs = Object.keys(body.translations);
+				if (languages.some((l) => !availableLangs.includes(l)))
+					throw new HTTPException(400, {
+						message: `Cannot find some of the requested languages: ${languages.join(", ")}`,
+					});
+
 				c.res = new Response(
 					JSON.stringify({
 						...body,
