7499e85be3cf5ddfd65f885fb5c7aa960c028bf6
Gregory Danielson <gregory.danielson3@gmail.com>
2024-01-31 08:28:20 -0600

    Add key construction callable to data types as a stop-gap

--- .github/workflows/package_data.py
+++ .github/workflows/package_data.py
@@ -1,30 +1,40 @@
 import json
 from pathlib import Path
-from typing import Any, Final, Iterator
+from typing import Any, Callable, Final, Iterator, Optional
 
 import tomlkit
 
 DATA_FOLDER: Final[str] = "metadata"
 TRANSLATIONS_FOLDER: Final[str] = "translations"
 
-DATA_TYPES: Final[set[str]] = {
-    "words",
-    "sandbox",
-    "luka_pona/signs",
-    "luka_pona/fingerspelling",
-    "fonts",
-    "languages",
+# Value is a function that produces the parent key, if any
+DATA_TYPES: Final[dict[str, Callable[[Path, dict], Optional[str]]]] = {
+    "words": lambda path, file: path.stem,
+    "sandbox": lambda path, file: path.stem,
+    "luka_pona/signs": lambda path, file: path.stem,
+    "luka_pona/fingerspelling": lambda path, file: path.stem,
+    "fonts": lambda path, file: path.stem,
+    "languages": lambda path, file: None,
 }
 
 
 def extract_data(
     result: dict[str, Any],
     paths: Iterator[Path],
+    key_maker: Callable[[Path, dict], Optional[str]],
 ):
     for path in paths:
         with open(path) as file:
             print(f"Reading {path}...")
-            result[path.stem] = tomlkit.load(file)
+            data = tomlkit.load(file)
+            key = key_maker(path, data)
+
+            if key:
+                result[key] = data
+                # we assume the key is unique
+            else:
+                result.update(data)
+                # we assume all keys in data are unique
 
 
 def insert_translations(result: dict[str, Any], paths: Iterator[Path]):
@@ -46,14 +56,16 @@ def insert_translations(result: dict[str, Any], paths: Iterator[Path]):
 
 
 if __name__ == "__main__":
-    for data_type in DATA_TYPES:
+    for data_type, transformer in DATA_TYPES.items():
         result: dict[str, Any] = {}
 
         extract_data(
             result,
             Path(".").glob(f"./{data_type}/{DATA_FOLDER}/*.toml"),
+            transformer,
         )
 
+        # doesn't need a transformer for now
         insert_translations(
             result,
             Path(".").glob(f"./{data_type}/{TRANSLATIONS_FOLDER}/*/*.toml"),
