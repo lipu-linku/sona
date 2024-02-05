d972f0e1180972e0110b978a273b0082b949565d
theonlytails <theonlytails@theonlytails.com>
2024-02-05 20:27:15 +0200

    allow any origin to access the api

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,12 +3,14 @@ import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 import { createZodFetcher } from "zod-fetch";
 import v1 from "./v1";
+import { cors } from "hono/cors";
 
 export const fetchWithZod = createZodFetcher();
 
 const app = new Hono({ strict: false })
 	.use("*", prettyJSON())
 	.use("*", logger())
+	.use("*", cors())
 	.notFound((c) => c.json({ message: "Not Found", ok: false }, 404))
 	.onError((err, c) => {
 		return c.json(
