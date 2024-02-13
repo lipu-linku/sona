447cc373faa33342ee5d48613f81a51b2dd786fe
Gregory Danielson <gregory.danielson3@gmail.com>
2024-02-13 10:55:41 -0600

    temp

--- update.py
+++ update.py
@@ -3,6 +3,8 @@ import os
 import argparse
 import tomlkit
 import logging
+from collections import defaultdict
+from functools import partial
 
 # what addition/change is this? words, sandbox, luka_pona/signs, luka_pona/fingerspellings, languages, fonts
 # is this a translation? y/n (doesn't apply to fonts/languages)
@@ -14,65 +16,42 @@ IJO_DIR = "/home/gregdan3/git/tokipona/ijo/sitelenpona/sitelen-seli-kiwen/"
 
 RESOURCE_DIR = "https://raw.githubusercontent.com/lipu-linku/ijo/main/sitelenpona/sitelen-seli-kiwen/"
 
-NOT_WRITTEN = {"li", "e", "la", "anu", "o", "pi", "a", "ali", "en"}
+
+def nested_defaultdict():
+    return defaultdict(partial(defaultdict, dict))
+
+
+def create_metadata(glyph: str):
+    data = dict()
+    data["creation_date"] = ""
+    data["author_commentary"] = ""
+    data["author_commentary_source"] = ""
+    data["usage"] = dict()
+    data["usage_category"] = ""
+    data["glyphs"] = dict()
+    data["glyphs"]["sitelen_seli_kiwen"] = dict()
+    data["glyphs"]["sitelen_seli_kiwen"]["svg"] = RESOURCE_DIR + glyph + ".svg"
+    data["glyphs"]["sitelen_seli_kiwen"]["png"] = RESOURCE_DIR + glyph + ".png"
+    return data
 
 
 def main(argv: argparse.Namespace):
     LOG.setLevel(argv.log_level)
-    tomls = [f for f in os.listdir(argv.directory) if f.endswith(".toml")]
-    for toml in tomls:
-        tomlname = os.path.join(argv.directory, toml)
-        print(tomlname)
-        with open(tomlname, "r") as f:
-            data = tomlkit.loads(f.read())
-            for key in {
-                "book",
-                "audio",
-                "etymology",
-                "ku_data",
-                "source_language",
-                "resources",
-                "representations",
-                "pu_verbatim",
-                "author_verbatim",
-                "author_verbatim_source",
-                "see_also",
-                "usage",
-                "usage_category",
-                "coined_year",
-            }:
-                if key in data:
-                    del data[key]
-                data["creation_date"] = ""
-                data["author_commentary"] = ""
-                data["author_commentary_source"] = ""
-                data["usage"] = dict()
-                data["usage_category"] = ""
+    glyphs = [f.rstrip(".svg") for f in os.listdir(IJO_DIR) if f.endswith(".svg")]
+    output = nested_defaultdict()
+    for glyph in glyphs:
+        if "-" not in glyph:
+            continue
 
-            glyphs = [
-                f.rstrip(".svg").rstrip(".png")
-                for f in os.listdir(IJO_DIR)
-                if f.endswith(".svg")
-            ]
-            for glyph in glyphs:
-                if not data["word"] == glyph[: glyph.find("-")]:
-                    continue
+        word = glyph[: glyph.find("-")]
 
-                data["glyphs"] = {}
-                data["glyphs"]["sitelen_seli_kiwen"] = {}
-                data["glyphs"]["sitelen_seli_kiwen"]["svg"] = (
-                    RESOURCE_DIR + glyph + ".svg"
-                )
-                data["glyphs"]["sitelen_seli_kiwen"]["png"] = (
-                    RESOURCE_DIR + glyph + ".png"
-                )
-                with open(f"glyphs/metadata/{glyph}.toml", "w") as dump:
-                    dump.write(tomlkit.dumps(data))
+        output[word]["primary"] = f"{word}-1"
+        output[word][glyph.replace("-", "_")] = create_metadata(glyph)
 
-            # edited_data = tomlkit.dumps(data)
-            # f.truncate(0)
-            # f.seek(0)
-            # f.write(edited_data)
+    for word, data in output.items():
+        print(word)
+        with open(f"glyphs/metadata/{word}.toml", "w") as dump:
+            dump.write(tomlkit.dumps(data, sort_keys=True))
 
 
 ### Typing utils for argparse
@@ -93,13 +72,6 @@ if __name__ == "__main__":
         dest="log_level",
         default="INFO",
         choices=["NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-    )
-    parser.add_argument(
-        "--directory",
-        help="Specify a directory of TOML files to update",
-        dest="directory",
-        required=True,
-        type=existing_directory,
     )
     ARGV = parser.parse_args()
     main(ARGV)
