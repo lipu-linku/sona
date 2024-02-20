eb4e9348858e4dc2ed7920a44370ef96aba83d7d
theonlytails <theonlytails@theonlytails.com>
2024-02-20 19:55:56 +0200

    this should work now

--- api/vite.config.ts
+++ api/vite.config.ts
@@ -1,47 +1,54 @@
 import pages from "@hono/vite-cloudflare-pages";
 import devServer from "@hono/vite-dev-server";
-import { defineConfig } from "vite";
+import { type UserConfigExport, defineConfig } from "vite";
 import { resolve } from "node:path";
 import dts from "vite-plugin-dts";
 import { exec } from "node:child_process";
 
-export default defineConfig({
-	build: {
-		lib: {
-			entry: {
-				index: "src/lib/index.ts",
-				utils: "src/lib/utils.ts",
-				client: "src/lib/client.ts",
+export default defineConfig((async ({ mode }) => {
+	return {
+		build: {
+			lib: mode === "lib" && {
+				entry: {
+					index: "src/lib/index.ts",
+					utils: "src/lib/utils.ts",
+					client: "src/lib/client.ts",
+				},
+				formats: ["es"],
 			},
-			formats: ["es"]
+			sourcemap: true,
+			minify: true,
+			outDir: "dist",
 		},
-		sourcemap: true,
-		minify: true,
-		outDir: "dist",
-	},
-	resolve: {
-		alias: {
-			$lib: resolve(__dirname, "src/lib"),
-			$server: resolve(__dirname, "src/server"),
+		resolve: {
+			alias: {
+				$lib: resolve(__dirname, "src/lib"),
+				$server: resolve(__dirname, "src/server"),
+			},
 		},
-	},
-	define: {
-		__BRANCH__: await new Promise<string>((resolve, reject) =>
-			exec("git branch --show-current", (e, stdout, stderr) => {
-				if (e) reject(`Could not get current branch: ${stderr}`);
-				else resolve(`"${stdout.trim()}"`);
-			}),
-		),
-	},
-	plugins: [
-		pages({
-			entry: "src/server/index.ts",
-		}),
-		devServer({
-			entry: "src/server/index.ts",
-		}),
-		dts({
-			insertTypesEntry: true,
-		}),
-	],
-});
+		define: {
+			__BRANCH__: await new Promise<string>((resolve, reject) =>
+				exec("git branch --show-current", (e, stdout, stderr) => {
+					if (e) reject(`Could not get current branch: ${stderr}`);
+					else resolve(`"${stdout.trim()}"`);
+				}),
+			),
+		},
+		plugins:
+			mode === "lib"
+				? [
+						dts({
+							include: "./src/lib/*.ts",
+							insertTypesEntry: true,
+						}),
+					]
+				: [
+						pages({
+							entry: "src/server/index.ts",
+						}),
+						devServer({
+							entry: "src/server/index.ts",
+						}),
+					],
+	};
+}) as UserConfigExport);
