1f7a92515231b93515e0400117e4f396ec785543
theonlytails <theonlytails@theonlytails.com>
2024-03-06 18:38:52 +0200

    fixed client function types

--- api/src/lib/client.ts
+++ api/src/lib/client.ts
@@ -1,15 +1,14 @@
 import { hc } from "hono/client";
 import type { AppType } from "$server";
 
-export const client = ({
-	baseUrl = "https://api.linku.la/",
-	fetchFunc = fetch,
-}: {
-	baseUrl: string;
-	fetchFunc: typeof fetch;
-}) =>
-	hc<AppType>(baseUrl, {
-		fetch: fetchFunc,
+export const client = (args?: {
+	baseUrl?: string;
+	fetch?: typeof fetch;
+}) => {
+	const { baseUrl, fetch } = args;
+	return hc<AppType>(baseUrl, {
+		fetch
 	});
+};
 
 export type ApiType = AppType;
