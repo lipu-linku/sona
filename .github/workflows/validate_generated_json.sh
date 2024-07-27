8d5f5f43111ba7ae386b704d5eafa4444c092566
Theodore Dubois <tblodt@icloud.com>
2024-07-27 15:35:38 -0700

    lon la nasin pi kulupu ijo li wile e ilo bash e ilo sh ala
    list=(ijo mute) la ijo mute li "kulupu", nasin ni li namako tan ilo
    namako bash.

--- .github/workflows/validate_generated_json.sh
+++ .github/workflows/validate_generated_json.sh
@@ -1,8 +1,8 @@
-#!/bin/sh -e
+#!/bin/bash -e
 list=(./api/raw/*.json)
 for file in "${list[@]}"; do
     generated="$(basename "${file}")"
-    if [ "$generated" == "sandbox.json" ]; then
+    if [[ "$generated" == "sandbox.json" ]]; then
         generated="words.json"
     fi
     npx ajv-cli -s "./api/generated/$generated" -c ajv-formats --all-errors --errors=text -d $file;
