e08a9a2b18969c23f9f8999c1763a31a59a0a050
Gregory Danielson <gregory.danielson3@gmail.com>
2025-05-04 16:16:50 -0500

    split packaging metadata into constants.py
    complete with nonsense import method so this works from any dir

---
+++ .github/workflows/constants.py
@@ -0,0 +1,130 @@
+from typing import Callable, Final, Literal, NotRequired, TypedDict
+
+DATA_FOLDER: Final[str] = "metadata"
+TRANSLATIONS_FOLDER: Final[str] = "translations"
+CURRENT_API_VERSION = "v2"
+
+Packager = Callable[[str, str, str], None]
+
+
+class Refs(TypedDict):
+    key: str
+    to: list[str]
+
+
+class DataToPackage(TypedDict):
+    input: str
+    output: str
+    type: Literal["data", "locales"]
+    translations: NotRequired[str]
+    refs: list[Refs]
+
+
+DATA: dict[str, DataToPackage] = {
+    "words": {
+        "input": "words/metadata/{id}.toml",
+        "output": "words.json",
+        "type": "data",
+        "translations": "words_locale",
+        "refs": [
+            {"key": "see_also", "to": ["words"]},
+            # {"key": "glyph_ids", "to": ["glyphs"]},
+            # {"key": "synonym_glyph_ids", "to": ["glyphs"]},
+        ],
+    },
+    "glyphs": {
+        "input": "glyphs/metadata/{id}.toml",
+        "output": "glyphs.json",
+        "type": "data",
+        "translations": "glyphs_locale",
+        "refs": [
+            {"key": "word_id", "to": ["words"]},
+        ],
+    },
+    "sandbox_words": {
+        "input": "sandbox/words/metadata/{id}.toml",
+        "output": "sandbox/words.json",
+        "type": "data",
+        "translations": "sandbox_words_locale",
+        "refs": [
+            # TODO: sandbox words can ref words...
+            {"key": "see_also", "to": ["words", "sandbox_words"]},
+            # {"key": "glyph_ids", "to": ["sandbox_glyphs"]},
+            # {"key": "synonym_glyph_ids", "to": ["sandbox_glyphs"]},
+        ],
+    },
+    "sandbox_glyphs": {
+        "input": "sandbox/glyphs/metadata/{id}.toml",
+        "output": "sandbox/glyphs.json",
+        "type": "data",
+        "translations": "sandbox_glyphs_locale",
+        "refs": [
+            {"key": "word_id", "to": ["words", "sandbox_words"]},
+        ],
+    },
+    "lp_signs": {
+        "input": "luka_pona/signs/metadata/{id}.toml",
+        "output": "luka_pona/signs.json",
+        "type": "data",
+        "translations": "lp_signs_locale",
+        "refs": [],
+    },
+    "lp_fingerspelling": {
+        "input": "luka_pona/fingerspelling/metadata/{id}.toml",
+        "output": "luka_pona/fingerspelling.json",
+        "type": "data",
+        "translations": "lp_fingerspelling_locale",
+        "refs": [],
+    },
+    "fonts": {
+        "input": "fonts/metadata/{id}.toml",
+        "output": "fonts.json",
+        "type": "data",
+        "refs": [],
+    },
+    "languages": {
+        "input": "languages/metadata/{id}.toml",
+        "output": "languages.json",
+        "type": "data",
+        "refs": [],
+    },
+    ###
+    ###
+    ###
+    "words_locale": {
+        "input": "words/translations/{langcode}/{id}.toml",
+        "output": "translations/{langcode}/words.json",
+        "type": "locales",
+        "refs": [],
+    },
+    "glyphs_locale": {
+        "input": "glyphs/translations/{langcode}/{id}.toml",
+        "output": "translations/{langcode}/glyphs.json",
+        "type": "locales",
+        "refs": [],
+    },
+    "sandbox_words_locale": {
+        "input": "sandbox/words/translations/{langcode}/{id}.toml",
+        "output": "sandbox/translations/{langcode}/words.json",
+        "type": "locales",
+        "refs": [],
+    },
+    "sandbox_glyphs_locale": {
+        "input": "sandbox/glyphs/translations/{langcode}/{id}.toml",
+        "output": "sandbox/translations/{langcode}/glyphs.json",
+        "type": "locales",
+        "refs": [],
+    },
+    "lp_signs_locale": {
+        "input": "luka_pona/signs/translations/{langcode}/{id}.toml",
+        "output": "luka_pona/translations/{langcode}/signs.json",
+        "type": "locales",
+        "refs": [],
+    },
+    "lp_fingerspelling_locale": {
+        "input": "luka_pona/fingerspelling/translations/{langcode}/{id}.toml",
+        "output": "luka_pona/translations/{langcode}/fingerspelling.json",
+        "type": "locales",
+        "refs": [],
+    },
+}
--- .github/workflows/package_data.py
+++ .github/workflows/package_data.py
@@ -1,21 +1,16 @@
 import json
+import os
 import re
+import sys
 import tomllib
 from collections import defaultdict
 from pathlib import Path
-from typing import Any, Callable, Final, TypedDict
 
-DATA_FOLDER: Final[str] = "metadata"
-TRANSLATIONS_FOLDER: Final[str] = "translations"
-CURRENT_API_VERSION = "v2"
+# this lets you import from files in the same dir
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(SCRIPT_DIR)
 
-Transformer = Callable[[str, str, str], None]
-
-
-class DataPendingTransform(TypedDict):
-    input: str
-    output: str
-    transformer: Transformer
+from constants import CURRENT_API_VERSION, DATA, Packager
 
 
 def make_singular(word: str) -> str:
@@ -41,7 +36,7 @@ def find_files(glob_pattern: str):
     return Path().glob(glob_path)
 
 
-def write_json(path: Path, data: dict[str, Any]):
+def write_json(path: Path, data):
     path.parent.mkdir(parents=True, exist_ok=True)
     raw_data = json.dumps(
         data,
@@ -67,26 +62,25 @@ def fetch_data(input: str, output: str) -> dict[str, dict]:
 
     data = defaultdict(lambda: defaultdict(defaultdict))
     for file in find_files(input):
-        # print(file)
+        print(file)
         path_str = str(file).replace("\\", "/")
         m = input_pattern.match(path_str)
         if not m:
             print(f"Path {path_str} does not match input pattern {input_pattern}")
             continue
         params = m.groupdict()
-
-        group_key = tuple(params[var] for var in output_vars)
         key = params[key_var]
+        # TODO: what if there are more intermediate layers?
 
         with file.open("rb") as f:
-            data[group_key][key] = tomllib.load(f)
+            data[key] = tomllib.load(f)
             # we intend to copy the item's id to the id key
             # not all files are set up this way at present...
             # TODO:
-            if key != data[group_key][key]["id"]:
+            if key != data[key]["id"]:
                 print(f"key-id mismatch in {file}")
 
-    return data
+    return dict(data)
 
 
 def package_data(root: str, input: str, output: str):
@@ -114,20 +108,25 @@ def fetch_locales(input: str, output: str) -> dict[str, dict]:
         params = m.groupdict()
 
         # expected to be one langcode per file right now
-        paths = tuple(params[var] for var in output_vars)
+        groups = tuple(params[var] for var in output_vars)
+        if len(groups) != 1:
+            raise ValueError(f"Expected exactly one locale data group, got {groups}")
+        group = next(iter(groups))
+        # TODO: what if there are more intermediate layers?
+
         # 'commentary', 'definition', etc
         keys = {k: v for k, v in params.items() if k not in output_vars}
         if len(keys) != 1:
             raise ValueError(f"Expected exactly one type of locale string, got {keys}")
-
         key = make_singular(next(iter(keys.values())))
+
         with file.open("rb") as f:
             local_data = tomllib.load(f)
             # 'a': 'emphasis particle' or whatever
             for object_id, locale_string in local_data.items():
-                data[paths][object_id][key] = locale_string
+                data[group][object_id][key] = locale_string
 
-    return data
+    return dict(data)
 
 
 def package_locales(root: str, input: str, output: str):
@@ -139,95 +138,18 @@ def package_locales(root: str, input: str, output: str):
         output_path = Path(root) / substitute_params(output, params)
         write_json(output_path, locale_data)
 
-    for paths, data in data.items():
-        params = dict(zip(output_vars, paths))
-        output_path = Path(root) / substitute_params(output, params)
-        write_json(output_path, data)
 
-
-DATA: dict[str, DataPendingTransform] = {
-    "words": {
-        "input": "words/metadata/{id}.toml",
-        "output": "words.json",
-        "transformer": package_data,
-    },
-    "glyphs": {
-        "input": "glyphs/metadata/{id}.toml",
-        "output": "glyphs.json",
-        "transformer": package_data,
-    },
-    "sandbox_words": {
-        "input": "sandbox/words/metadata/{id}.toml",
-        "output": "sandbox/words.json",
-        "transformer": package_data,
-    },
-    "sandbox_glyphs": {
-        "input": "sandbox/glyphs/metadata/{id}.toml",
-        "output": "sandbox/glyphs.json",
-        "transformer": package_data,
-    },
-    "lp_signs": {
-        "input": "luka_pona/signs/metadata/{id}.toml",
-        "output": "luka_pona/signs.json",
-        "transformer": package_data,
-    },
-    "lp_fingerspelling": {
-        "input": "luka_pona/fingerspelling/metadata/{id}.toml",
-        "output": "luka_pona/fingerspelling.json",
-        "transformer": package_data,
-    },
-    "fonts": {
-        "input": "fonts/metadata/{id}.toml",
-        "output": "fonts.json",
-        "transformer": package_data,
-    },
-    "languages": {
-        "input": "languages/metadata/{id}.toml",
-        "output": "languages.json",
-        "transformer": package_data,
-    },
-    ###
-    ###
-    ###
-    "words_locale": {
-        "input": "words/translations/{langcode}/{id}.toml",
-        "output": "translations/{langcode}/words.json",
-        "transformer": package_locales,
-    },
-    "glyphs_locale": {
-        "input": "glyphs/translations/{langcode}/{id}.toml",
-        "output": "translations/{langcode}/glyphs.json",
-        "transformer": package_locales,
-    },
-    "sandbox_words_locale": {
-        "input": "sandbox/words/translations/{langcode}/{id}.toml",
-        "output": "sandbox/translations/{langcode}/words.json",
-        "transformer": package_locales,
-    },
-    "sandbox_glyphs_locale": {
-        "input": "sandbox/glyphs/translations/{langcode}/{id}.toml",
-        "output": "sandbox/translations/{langcode}/glyphs.json",
-        "transformer": package_locales,
-    },
-    "lp_signs_locale": {
-        "input": "luka_pona/signs/translations/{langcode}/{id}.toml",
-        "output": "luka_pona/translations/{langcode}/signs.json",
-        "transformer": package_locales,
-    },
-    "lp_fingerspelling_locale": {
-        "input": "luka_pona/fingerspelling/translations/{langcode}/{id}.toml",
-        "output": "luka_pona/translations/{langcode}/fingerspelling.json",
-        "transformer": package_locales,
-    },
-}
+FETCH_MAP = {"data": fetch_data, "locales": fetch_locales}
+PACKAGE_MAP: dict[str, Packager] = {"data": package_data, "locales": package_locales}
 
 
 def main():
     for id, metadata in DATA.items():
         input = metadata["input"]
         output = metadata["output"]
-        transformer = metadata["transformer"]
-        transformer(f"api/raw/{CURRENT_API_VERSION}/", input, output)
+        typ = metadata["type"]
+        packager = PACKAGE_MAP[typ]
+        packager(f"api/raw/{CURRENT_API_VERSION}/", input, output)
 
     print("Done!")
 
