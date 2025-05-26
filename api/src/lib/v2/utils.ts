2082692e27622e548c5ac6d821fb733913df7369
Gregory Danielson <gregory.danielson3@gmail.com>
2025-05-26 15:06:32 -0500

    replace unions with enums in v2 utils

--- api/src/lib/v2/utils.ts
+++ api/src/lib/v2/utils.ts
@@ -4,23 +4,27 @@ export const OptionalDate = z
 	.string()
 	.regex(/^(20\d{2}-(0[1-9]|1[0-2])(-(0[1-9]|[12]\d|3[01]))?)?$/);
 
-export const Book = z.union([
-	z.literal("pu"),
-	z.literal("ku suli"),
-	z.literal("ku lili"),
-	z.literal("none"),
+export const Book = z.enum([
+	"pu",
+	"ku suli",
+	"ku lili",
+	"none", // mu
 ]);
 export type Book = z.infer<typeof Book>;
 
-export const CoinedEra = z.union([z.literal("pre-pu"), z.literal("post-pu"), z.literal("post-ku")]);
+export const CoinedEra = z.enum([
+	"pre-pu",
+	"post-pu",
+	"post-ku", // mu
+]);
 export type CoinedEra = z.infer<typeof CoinedEra>;
 
-export const UsageCategory = z.union([
-	z.literal("core"),
-	z.literal("common"),
-	z.literal("uncommon"),
-	z.literal("obscure"),
-	z.literal("sandbox"),
+export const UsageCategory = z.enum([
+	"core",
+	"common",
+	"uncommon",
+	"obscure",
+	"sandbox", // mu
 ]);
 export type UsageCategory = z.infer<typeof UsageCategory>;
 
