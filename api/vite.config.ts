b141769067f93abfcd44e0b99650d9619edf9bd5
Gregory Danielson <gregory.danielson3@gmail.com>
2025-06-01 18:23:11 -0500

    remove unused branch definition from vite config

--- api/vite.config.ts
+++ api/vite.config.ts
@@ -1,45 +1,36 @@
 import pages from "@hono/vite-cloudflare-pages";
 import devServer from "@hono/vite-dev-server";
-import { exec } from "node:child_process";
 import { defineConfig, type UserConfigExport } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig((async ({ mode }) => ({
-	build: {
-		lib: mode === "lib" && {
-			entry: {
-				index: "src/lib/index.ts",
-				utils: "src/lib/utils.ts",
-				client: "src/lib/client.ts",
-			},
-			formats: ["es"],
-		},
-		sourcemap: "inline",
-		minify: true,
-		outDir: "dist",
-	},
-	define: {
-		__BRANCH__: await new Promise<string>((resolve, reject) =>
-			exec("git branch --show-current", (e, stdout, stderr) => {
-				if (e) reject(`Could not get current branch: ${stderr}`);
-				else resolve(`"${stdout.trim()}"`);
-			}),
-		),
-	},
-	plugins:
-		mode === "lib"
-			? [
-					dts({
-						include: "./src/**/*.ts",
-						insertTypesEntry: true,
-					}),
-				]
-			: [
-					pages({
-						entry: "src/server/index.ts",
-					}),
-					devServer({
-						entry: "src/server/index.ts",
-					}),
-				],
+  build: {
+    lib: mode === "lib" && {
+      entry: {
+        index: "src/lib/index.ts",
+        utils: "src/lib/utils.ts",
+        client: "src/lib/client.ts",
+      },
+      formats: ["es"],
+    },
+    sourcemap: "inline",
+    minify: true,
+    outDir: "dist",
+  },
+  plugins:
+    mode === "lib"
+      ? [
+          dts({
+            include: "./src/**/*.ts",
+            insertTypesEntry: true,
+          }),
+        ]
+      : [
+          pages({
+            entry: "src/server/index.ts",
+          }),
+          devServer({
+            entry: "src/server/index.ts",
+          }),
+        ],
 })) as UserConfigExport);
