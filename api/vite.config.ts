724071d89fd591db288cbfe4f4949581fb77a84a
Gregory Danielson <gregory.danielson3@gmail.com>
2026-02-27 10:08:58 -0600

    fix: tell vite what entrypoints the library has

--- api/vite.config.ts
+++ api/vite.config.ts
@@ -1,12 +1,33 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
-import { defineConfig } from "vite";
 import path from "path";
+import { defineConfig, type UserConfigExport } from "vite";
 
-export default defineConfig({
-  plugins: [cloudflare()],
+export default defineConfig((async ({ mode }) => ({
+  build: {
+    lib: mode === "lib" && {
+      entry: {
+        index: "src/lib/v1/index.ts",
+        utils: "src/lib/v1/utils.ts",
+        client: "src/lib/v1/client.ts",
+
+        "v1/index": "src/lib/v1/index.ts",
+        "v1/utils": "src/lib/v1/utils.ts",
+        "v1/client": "src/lib/v1/client.ts",
+
+        "v2/index": "src/lib/v2/index.ts",
+        "v2/utils": "src/lib/v2/utils.ts",
+        "v2/client": "src/lib/v2/client.ts",
+      },
+      formats: ["es"],
+    },
+    sourcemap: "inline",
+    minify: true,
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@raw": path.resolve(__dirname, "src/raw"),
     },
   },
-});
+  plugins: [cloudflare()],
+})) as UserConfigExport);
