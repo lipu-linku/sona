7efd0a73dc807f5cd219d45648a1a2b02e48d12d
Gregory Danielson <gregory.danielson3@gmail.com>
2026-02-27 09:58:58 -0600

    fix: SignData replaced with Sign

--- api/src/lib/v2/types.ts
+++ api/src/lib/v2/types.ts
@@ -1,4 +1,4 @@
-import type { Font, Word, SignData } from "./index";
+import type { Font, Word, Sign } from "./index";
 
 export type WordRepresentations = Word["representations"];
 export type WordAudio = Word["audio"];
@@ -9,11 +9,10 @@ export type WordTranslations = Word["translations"];
 export type LocalizedWordDefinition = WordTranslations["definition"];
 export type LocalizedWordEtymology = WordTranslations["etymology"];
 export type LocalizedWordCommentary = WordTranslations["commentary"];
-// export type LocalizedWordSitelenEtymology = WordTranslations["sp_etymology"];
 
-export type SignEtymology = SignData["etymology"];
-export type SignWriting = SignData["signwriting"];
-export type SignVideo = SignData["video"];
+export type SignEtymology = Sign["etymology"];
+export type SignWriting = Sign["signwriting"];
+export type SignVideo = Sign["video"];
 // export type LocalizedSignParameters = ParametersTranslation[string];
 
 export type FontLinks = Font["links"];
